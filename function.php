3ecdb7f6c7da4ee3ba1351df409dbe52791bad8b
ZanetaBor <biurozanweb@gmail.com>
2021-02-22 17:02:05 +0100

    update function

--- function.php
+++ function.php
@@ -2,9 +2,9 @@
 
 // gets date of birth from the pesel number
 function peselNumb($pesel) {
-    $year=substr($pesel,4,4);
+    $year=substr($pesel,0,2);
     $month=substr($pesel,2,2);	
-    $day=substr($pesel,0,2);	
+    $day=substr($pesel,4,2);	
     echo "year of birth:  ".$year."</br>";	
     echo "month of birth:  ".$month."</br>";
     echo "day of birth:  ".$day;
